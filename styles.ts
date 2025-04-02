88797d4db13e6e3b1ec5304bd66796816b577e85
Camile Oliveira <143637837+camileoliv@users.noreply.github.com>
2025-04-02 14:55:57 -0300

    Add files via upload

---
+++ styles.ts
@@ -0,0 +1,40 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#D7E9FF",
+        padding: 20,
+    },
+    container2: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#D7E9FF",
+        padding: 20,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: "bold",
+        color: "#efb5c2",
+        marginBottom: 20,
+    },
+    input: {
+        width: "80%",
+        padding: 10,
+        marginVertical: 10,
+        borderWidth: 1,
+        borderColor: "#b2dfdb",
+        borderRadius: 5,
+        backgroundColor: "#ffffff",
+        borderRadius: 150,
+    },
+    result: {
+        marginTop: 15,
+        fontSize: 16,
+        fontWeight: "bold",
+        color: "#004d40",
+    },
+});
